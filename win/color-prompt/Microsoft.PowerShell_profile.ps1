4f0cf9280eca702e78022b45dec85dab895170a3
adam.szi <adam.szi@lightware.com>
2018-07-09 17:34:06 +0200

    updated powershell coloring

--- win/color-prompt/Microsoft.PowerShell_profile.ps1
+++ win/color-prompt/Microsoft.PowerShell_profile.ps1
@@ -11,12 +11,37 @@ function getGitBranch {
     else { return "" }
 }
 
+function getDrive {
+    $dir = (Get-Item -Path ".\").FullName
+    return $dir.Split(':')[0]
+}
+
+function getHost {
+    $hostname = $Env:computername
+    $hostname = $hostname.ToLower()
+    $hostname = $hostname+".lwr.local"
+    return $hostname
+}
+
+function getPath {
+    $dir = (Get-Item -Path ".\").FullName
+    If($dir.StartsWith($HOME)){
+        $dir = $dir.Replace($HOME, '~')
+    } else {
+        $dir = "/Volumes/" + $dir
+    }
+    $dir = $dir.Replace(':', '')
+    $dir = $dir.Replace('\', '/')
+    
+    return $dir
+}
+
 function Global:prompt {
-    Write-Host "ps " -nonewline
-    Write-Host $Env:username -nonewline -foreground Green
-    Write-Host ":" -nonewline
-    Write-Host (Get-Item -Path ".\").FullName -nonewline -foreground Blue
+    #Write-Host "ps " -nonewline
+    Write-Host $Env:username@$(getHost) -nonewline -foreground Green
+    Write-Host ":" -nonewline -foreground White
+    Write-Host $(getPath) -nonewline -foreground Blue
     Write-Host $(getGitBranch) -nonewline -foreground Red
-    Write-Host " $" -nonewline
+    Write-Host "$" -nonewline -foreground White
     return " "
 }
